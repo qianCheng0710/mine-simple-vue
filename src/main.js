be64751aee960f1539e66897ee1ca4bbb07cb220
qiancheng <1035095466qq.com>
2019-10-17 15:50:23 +0800

    默认模板修改部分错误

--- src/main.js
+++ src/main.js
@@ -13,11 +13,10 @@ import './directives';
 // 过滤器
 import './filters';
 
-
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
